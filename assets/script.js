3a28e41c6555b870ed62bef685b541d50cd780f8
KholifAl <kholif.alham@gmail.com>
2023-10-27 14:24:27 +0700

    kata

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const slider = document.querySelector(".slider");
-    const arrowLeft = document.querySelector(".arrow-left");
-    const arrowRight = document.querySelector(".arrow-right");
-
-    let currentIndex = 0;
-
-    arrowLeft.addEventListener("click", function() {
-        if (currentIndex > 0) {
-            currentIndex--;
-            slide();
-        }
+const carousel = document.querySelector(".carousel"),
+firstImg = carousel.querySelectorAll("img")[0],
+arrowIcons = document.querySelectorAll(".wrapper i");
+let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+const showHideIcons = () => {
+    // showing and hiding prev/next icon according to carousel scroll left value
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+}
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
+        // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
     });
-
-    arrowRight.addEventListener("click", function() {
-        if (currentIndex < slider.children.length - 1) {
-            currentIndex++;
-            slide();
-        }
-    });
-
-    function slide() {
-        const cardWidth = slider.querySelector(".card").offsetWidth;
-        const offset = -currentIndex * cardWidth;
-        slider.style.transform = `translateX(${offset}px)`;
-    }
 });
