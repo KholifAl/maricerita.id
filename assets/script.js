18d6983578266389af55e140407b1c52db7f32ca
KholifAl <kholif.alham@gmail.com>
2023-11-03 08:47:33 +0700

    cmt

--- assets/script.js
+++ assets/script.js
@@ -57,3 +57,21 @@ scrollUpButton.addEventListener("click", () => {
 scrollDownButton.addEventListener("click", () => {
     container.scrollTop += 200; // Adjust the scroll distance as needed
 });
+
+
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function (e) {
+        // Prevent the default link behavior
+        e.preventDefault();
+
+        // Remove the "active" class from all links
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+        });
+
+        // Add the "active" class to the clicked link
+        this.classList.add('active');
+    });
+});
